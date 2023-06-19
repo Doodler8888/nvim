0d9572e990f9b20ce786b79f280d7b77d30b4302
Doodler8888 <113293653+Doodler8888@users.noreply.github.com>
2023-06-19 16:03:44 +0300

    Update init.lua

--- init.lua
+++ init.lua
@@ -3,6 +3,9 @@ require("wurfkreuz")
 vim.cmd('colorscheme rose-pine')
 vim.o.clipboard = "unnamedplus"
 
+--  Prevent auto commenting on new lines
+vim.cmd([[autocmd FileType * setlocal formatoptions-=c formatoptions-=r formatoptions-=o]])
+
 vim.api.nvim_exec([[
   autocmd BufNewFile,BufRead *.tf setfiletype terraform
 ]], false)
